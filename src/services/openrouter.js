5dee3db5757bb6002fc02d2825b336d4828f90dd
Jorge1048 <andresvalenciajorge@gmail.com>
2025-06-01 12:33:01 -0500

    Update openrouter.js

--- src/services/openrouter.js
+++ src/services/openrouter.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const OPENROUTER_API_KEY = "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";
+const OPENROUTER_API_KEY = "Tu_clave_API_quí";
 
 async function askOpenRouter(prompt) {
   if (!prompt) throw new Error("Prompt não fornecido!");
