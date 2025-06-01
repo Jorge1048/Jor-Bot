61bd9ed983fac15554d7fd27ae5d4e39e3ab95ee
Jorge1048 <andresvalenciajorge@gmail.com>
2025-06-01 08:14:55 -0500

    Update revelar.js

--- src/commands/admin/revelar.js
+++ src/commands/admin/revelar.js
@@ -8,7 +8,7 @@ const { getRandomName } = require(`${BASE_DIR}/utils`);
 module.exports = {
   name: "revelar",
   description: "Revela uma imagem ou vídeo com visualização única",
-  commands: ["revelar", "rv", "reveal"],
+  commands: ["revelarrrr", "rvrrr", "revealllll"],
   usage: `${DEFAULT_PREFIX}revelar (marque a imagem/vídeo) ou ${DEFAULT_PREFIX}revelar (responda a imagem/vídeo).`,
   /**
    * @param {CommandHandleProps} props
