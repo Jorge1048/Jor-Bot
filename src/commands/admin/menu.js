1b14234e52e044d4370db6c39cb260ce94c90d7c
Jorge1048 <andresvalenciajorge@gmail.com>
2025-06-03 03:21:07 -0500

    Update menu.js

--- src/commands/admin/menu.js
+++ src/commands/admin/menu.js
@@ -1,10 +1,11 @@
 const { PREFIX, ASSETS_DIR } = require(`${BASE_DIR}/config`);
 const { menuMessage } = require(`${BASE_DIR}/utils/messages`);
 const path = require("path");
+const { delay } = require("baileys");
 
 module.exports = {
   name: "menu",
-  description: "Menu de comandos",
+  description: "Menú de comandos",
   type: "admin",
   commands: ["menu", "helppppp"],
   usage: `${PREFIX}menu`,
@@ -12,12 +13,21 @@ module.exports = {
    * @param {CommandHandleProps} props
    * @returns {Promise<void>}
    */
-  handle: async ({ sendImageFromFile, sendSuccessReact }) => {
-    await sendSuccessReact();
+  handle: async ({ sendImageFromFile, sendReply, sendReact }) => {
+    // Reacciona para indicar recepción de comando
+    await sendReact("✅");
 
+    // Envía la imagen junto con el texto del menú
     await sendImageFromFile(
       path.join(ASSETS_DIR, "images", "takeshi-bot.png"),
       `\n\n${menuMessage()}`
     );
+
+    // Espera 2 segundos y luego envía el mensaje reflexivo como respuesta
+    setTimeout(async () => {
+      await sendReply(
+        "¡Hola! Usar el bot sin motivo puede afectar su funcionamiento. Te pedimos que lo uses solo cuando sea necesario.\n\nGracias por tu comprensión. 😊"
+      );
+    }, 15000);
   },
 };
