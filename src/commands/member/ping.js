7d9aabb5e721057b9cd63bee17213886553ee065
Jorge Andrés <andresvalenciajorge@gmail.com>
2025-07-12 17:45:24 -0500

    Update ping.js

--- src/commands/member/ping.js
+++ src/commands/member/ping.js
@@ -2,15 +2,20 @@ const { PREFIX } = require(`${BASE_DIR}/config`);
 
 module.exports = {
   name: "ping",
-  description: "Verificar se o bot está online",
+  description: "Verificar si el bot está online y mostrar el tiempo de respuesta",
   commands: ["ping"],
   usage: `${PREFIX}ping`,
+
   /**
    * @param {CommandHandleProps} props
    * @returns {Promise<void>}
    */
   handle: async ({ sendReply, sendReact }) => {
+    const start = Date.now();
     await sendReact("🏓");
-    await sendReply(`🏓 Pong!`);
+    const end = Date.now();
+    const ping = end - start;
+
+    await sendReply(`🏓 Pong!\n> Tiempo ⴵ ${ping}ms`);
   },
 };
